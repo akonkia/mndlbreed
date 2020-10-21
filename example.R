0c56e41338241f98c192293d32a53077159ccd37
Agnieszka Konkolewska <68012357+akonkia@users.noreply.github.com>
2020-10-21 09:54:09 +0100

    Add files via upload

--- example.R
+++ example.R
@@ -7,56 +7,45 @@ library(lattice)
 
 #User input
 offspring <- 100 # number of offspring to be generated
-t <- sample(alleles, 2) #  Number of traits to be taken. Up to 7 traits available.  For now, we assume that one trait goes on one individual chromosome.
-
-#alleles[alleles %in% t]
-
-
+t <- sample(alleles, 7) #  Number of traits to be taken. Up to 7 traits available.  For now, we assume that one trait goes on one individual chromosome.
 
 # Start with creating two dragon parents
 drag1 <- createDragon(t)
 drag2 <- createDragon(t)
 
 # Plot genotypes of the parents
-showParents(drag1, drag2)
+showPair(drag1, drag2)
 
-#create a sward of dragons, offspring (n) of the selected parents
+# create a sward of dragons, offspring (n) of the selected parents. Outputs plot under 4 traits, and table over 3 traits. Need to modify.
 sward <- makeSward(drag1, drag2, offspring)
 
-# Plot a selected dragon from the sward
-plotGenotype(sward[[2]])
+# Create summary of the sward genotypes
+showGenFreq(sward)
 
-# Show frequencies of the genotypes
+#Make a plot of the sward genotypes
+plotGenFreq(sward)
+
+#Create a table of the sward genotypes
+dim(getFreq(sward))
+
+# Plot a selected dragon from the sward. You can add a name as a second argument.
+plotGenotype(sward[[2]], "Elvis")
+
+# Show frequencies of the genotypes (table)
 freq <- getFreq(sward)
 
-
-min <- freq[freq$Count == min(freq$Count),]
-
-paste0("The rarest dragon genotype(s):\n ", paste(min$Genotype, collapse = ", "),"\n was/were spotted ", min$Count[1], " time(s).")
-max <- freq[freq$Count == max(freq$Count),]
-
-paste0("In the population of dragon babies, the most often occurring genotype(s):\n ", paste(max$Genotype, collapse = ", ")," was/were spotted ", max$Count[1], " time(s).")
-
-
-
-######Get phenotype of a dragon
+# Get phenotype of a dragon
 
 getPhenotype(sward[[3]])
 
-#Print photypic table
+# Print the phenotype table
 phenoTable
 
-#unique(phenoTable$Phenotype)
-collapsed <- collapsePhenoTable(phenoTable)
+# Collapse phenotype table (ex. put Aa and AA in the same cell, related to the same phenotype)
+collapsePhenoTable(phenoTable)
 
-shortenPhenoTable(t)
-
-
-type <- c(unlist(strsplit(collapseGenotype(t), " ")))
-type <- c(type, tolower(type))
-short <- phenoTable$Phenotype[phenoTable$Genotype %in% type]
-shortTable <- phenoTable[phenoTable$Phenotype %in% short,]
-shortTable <- collapsePhenoTable(shortTable)
+# Create a short collapsed phenotype table, containing only traits chosen for the simulation
+shortTable <- shortenPhenoTable(t)
 
 # Show frequencies of the phenotypes
 showPhenFreq(sward)
